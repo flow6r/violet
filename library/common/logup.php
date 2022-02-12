0e43c90fc328e769c313ebdcb548994d47de0578
m66rk9t <m66rk9t@gmail.com>
2022-02-12 13:34:11 +0800

    Upload logup.php

---
+++ library/common/logup.php
@@ -0,0 +1,31 @@
+<?php
+/*获取表单域信息*/
+//获取用户输入的信息
+$userID = $_POST["id"];
+$name = $_POST["name"];
+$passwd = $_POST["passwd"];
+//加密密码
+$passwd = password_hash($passwd, PASSWORD_BCRYPT);
+
+/*连接数据库，插入用户数据*/
+//连接数据库
+$db = mysqli_connect("localhost", "studenttest", "studenttest123", "EEMS");
+//检查连接
+if (mysqli_connect_error()) {
+    echo "<script type='text/javascript'>alert('错误：连接数据库失败，请联系管理员并反馈问题。');</script>";
+    // echo "<script type='text/javascript'>window.location.href='https://www.flow6r.fun/';</script>";
+    echo "<script type='text/javascript'>window.location.href='../../login.html';</script>";
+}
+//设置查询语句
+$query = "INSERT INTO Users_STD VALUES (?, ?, ?, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";
+$stmt = $db->prepare($query);
+//绑定查询参数
+$stmt->bind_param("sss", $userID, $name, $passwd);
+//执行查询
+$stmt->execute();
+//关闭链接
+$db->close();
+
+//注册成功，跳转至用户面板页面
+header("location:../../pages/panel.html");
+?>
