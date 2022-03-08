02ba46fc613e03a90b64399174539d43f42bfd82
m66rk9t <m66rk9t@gmail.com>
2022-03-08 16:09:54 +0800

    Update library/common/update_eqpt_info.php

--- library/common/update_eqpt_info.php
+++ library/common/update_eqpt_info.php
@@ -5,6 +5,7 @@ $currUserRole = $_POST["currUserRole"];
 $eqptNewID = $_POST["eqptNewID"];
 $eqptOldID = $_POST["eqptOldID"];
 $eqptNewName = $_POST["eqptNewName"];
+$eqptNewCls = $_POST["eqptNewCls"];
 $eqptNewColg = $_POST["eqptNewColg"];
 $eqptNewCre = $_POST["eqptNewCre"];
 $eqptNewDesc = $_POST["eqptNewDesc"];
@@ -12,9 +13,15 @@ $docRoot = $_SERVER["DOCUMENT_ROOT"];
 
 //引入数据库用户信息脚本
 switch ($currUserRole) {
-    case "学生":require_once("../dbuser/student.php");break;
-    case "教师":require_once("../dbuser/teacher.php");break;
-    case "管理员":require_once("../dbuser/admin.php");break;
+    case "学生":
+        require_once("../dbuser/student.php");
+        break;
+    case "教师":
+        require_once("../dbuser/teacher.php");
+        break;
+    case "管理员":
+        require_once("../dbuser/admin.php");
+        break;
 }
 
 //连接数据库
@@ -25,33 +32,75 @@ if (mysqli_connect_error()) {
 }
 
 //查询数据库
-$query = "SELECT EqptID FROM Equipments WHERE EqptID = ?";
-$stmt = $db->prepare($query);
-$stmt->bind_param("s", $eqptNewID);
-$stmt->execute();
-$stmt->store_result();
-if ($stmt->num_rows()) echo "1";
-else {
-    //查询学院信息
-    $query = "SELECT ColgName FROM Colleges WHERE ColgAbrv = ?";
+if ($eqptNewID === $eqptOldID) {
+    //无需更新设备ID，更新其余信息
+    //查询设备分类信息
+    $query = "SELECT ClsName FROM Classification WHERE ClsAbrv = ?";
     $stmt = $db->prepare($query);
-    $stmt->bind_param("s", $eqptNewColg);
+    $stmt->bind_param("s", $eqptNewCls);
     $stmt->execute();
     $stmt->store_result();
     if ($stmt->num_rows()) {
-        $stmt->bind_result($eqptNewColg);
+        $stmt->bind_result($eqptNewCls);
         $stmt->fetch();
-        //更新设备信息
-        $query = "UPDATE Equipments SET EqptID = ?, EqptName = ?, ColgName = ?, EqptCre = ?, EqptDesc = ? WHERE EqptID = ?";
+        //查询学院信息
+        $query = "SELECT ColgName FROM Colleges WHERE ColgAbrv = ?";
         $stmt = $db->prepare($query);
-        $stmt->bind_param("ssssss", $eqptNewID, $eqptNewName, $eqptNewColg, $eqptNewCre, $eqptNewDesc, $eqptOldID);
+        $stmt->bind_param("s", $eqptNewColg);
         $stmt->execute();
-        //更新设备图片文件名
-        rename($docRoot."/images/eqpts/".$eqptOldID.".jpg", $docRoot."/images/eqpts/".$eqptNewID.".jpg");
+        $stmt->store_result();
+        if ($stmt->num_rows()) {
+            $stmt->bind_result($eqptNewColg);
+            $stmt->fetch();
+            //更新设备信息
+            $query = "UPDATE Equipments SET EqptID = ?, EqptName = ?, ClsName = ?, ColgName = ?, EqptCre = ?, EqptDesc = ? WHERE EqptID = ?";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param("sssssss", $eqptNewID, $eqptNewName, $eqptNewCls, $eqptNewColg, $eqptNewCre, $eqptNewDesc, $eqptOldID);
+            $stmt->execute();
+            //更新设备图片文件名
+            rename($docRoot . "/images/eqpts/" . $eqptOldID . ".jpg", $docRoot . "/images/eqpts/" . $eqptNewID . ".jpg");
+        } else echo "3";
     } else echo "2";
+} else {
+    //更新ID，需要检测是否有同ID设备
+    $query = "SELECT EqptID FROM Equipments WHERE EqptID = ?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s", $eqptNewID);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows()) echo "1";
+    else {
+        $query = "SELECT ClsName FROM Classification WHERE ClsAbrv = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("s", $eqptNewCls);
+        $stmt->execute();
+        $stmt->store_result();
+        if ($stmt->num_rows()) {
+            $stmt->bind_result($eqptNewCls);
+            $stmt->fetch();
+            //查询学院信息
+            $query = "SELECT ColgName FROM Colleges WHERE ColgAbrv = ?";
+            $stmt = $db->prepare($query);
+            $stmt->bind_param("s", $eqptNewColg);
+            $stmt->execute();
+            $stmt->store_result();
+            if ($stmt->num_rows()) {
+                $stmt->bind_result($eqptNewColg);
+                $stmt->fetch();
+                //更新设备信息
+                $query = "UPDATE Equipments SET EqptID = ?, EqptName = ?, ClsName = ?, ColgName = ?, EqptCre = ?, EqptDesc = ? WHERE EqptID = ?";
+                $stmt = $db->prepare($query);
+                $stmt->bind_param("sssssss", $eqptNewID, $eqptNewName, $eqptNewCls, $eqptNewColg, $eqptNewCre, $eqptNewDesc, $eqptOldID);
+                $stmt->execute();
+                //更新设备图片文件名
+                rename($docRoot . "/images/eqpts/" . $eqptOldID . ".jpg", $docRoot . "/images/eqpts/" . $eqptNewID . ".jpg");
+            } else echo "3";
+        } else echo "2";
+    }
 }
 
 //释放结果集并关闭链接
 $stmt->free_result();
 $db->close();
+exit;
 ?>
