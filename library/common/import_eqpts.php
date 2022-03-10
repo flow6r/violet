f88c662e9356e97ec4c39c87bfbb83f6411e5d47
m66rk9t <m66rk9t@gmail.com>
2022-03-10 21:41:04 +0800

    Update library/common/import_eqpts.php

--- library/common/import_eqpts.php
+++ library/common/import_eqpts.php
@@ -20,8 +20,10 @@ if ($_FILES["newEqptsInfoTmpl"]["error"] === 1) {
 }
 
 //检查文件类型
-if ($_FILES["newEqptsInfoTmpl"]["type"] != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    && $_FILES["newEqptsInfoTmpl"]["type"] != "application/vnd.ms-excel") {
+if (
+    $_FILES["newEqptsInfoTmpl"]["type"] != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    && $_FILES["newEqptsInfoTmpl"]["type"] != "application/vnd.ms-excel"
+) {
     echo "<script>alert('请上传xls或xlsx格式的设备信息文件');</script>";
     exit;
 }
@@ -32,7 +34,7 @@ $userRole = $_SESSION["userInfo"]["userRole"];
 $userName = $_SESSION["userInfo"]["userName"];
 
 //设置文件基本名
-$baseName = "EqptInfoTmpl_".$userName.$currDateTime;
+$baseName = "EqptInfoTmpl_" . $userName . $currDateTime;
 
 //设置文件后缀名
 $extName = null;
@@ -40,15 +42,13 @@ if ($_FILES["newEqptsInfoTmpl"]["type"] === "application/vnd.ms-excel") $extName
 else $extName = ".xlsx";
 
 //设置文件路径
-$trgtPath = $docRoot."/data/upload/".$baseName.$extName;
+$trgtPath = $docRoot . "/data/upload/" . $baseName . $extName;
 
 //移动文件至指定目录
 if (is_uploaded_file($_FILES["newEqptsInfoTmpl"]["tmp_name"])) {
     if (!move_uploaded_file($_FILES["newEqptsInfoTmpl"]["tmp_name"], $trgtPath)) {
         echo "<script>alert('移动设备信息文件时发生错误，请联系管理员并反馈问题');</script>";
         exit;
-    } else {
-        echo "<script>alert('成功导入设备');</script>";
     }
 }
 
@@ -75,7 +75,63 @@ for ($outer = 0; $outer < $dataRows; $outer++, $begnRows++) {
 // $newEqptsInfoJSON = json_encode($newEqptsInfoAray, JSON_UNESCAPED_UNICODE);
 // file_put_contents("newEqptsInfo.json", $newEqptsInfoJSON);
 
-//检查数据表中是否包含已在库的设备
+//引入数据库用户信息脚本
+switch ($userRole) {
+    case "教师":
+        require_once("../dbuser/teacher.php");
+        break;
+    case "管理员":
+        require_once("../dbuser/admin.php");
+        break;
+}
 
+//连接数据库
+$db = mysqli_connect($dbServer, $dbUser, $dbUserPasswd, $dbName);
+if (mysqli_connect_error()) {
+    echo "<script>alert('连接数据库时发生错误，请联系管理员并反馈问题');</script>";
+    exit;
+}
+
+//检查数据表中是否包含已在库的设备
+$isExist = false;
+for ($indx = 0; $indx < count($newEqptsInfoAray); $indx++) {
+    $query = "SELECT EqptID FROM Equipments WHERE EqptID = ?";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param("s", $newEqptsInfoAray[$indx]["newEqptID"]);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows()) {
+        echo "<script>alert('您所导入的设备信息中，包含已在库的设备，请检查无误后继续导入');</script>";
+        $isExist = true;
+        break;
+    }
+}
+
+//将设备信息插入数据库中
+if (!$isExist) {
+    $eqptStat = "未借用";
+
+    for ($indx = 0; $indx < count($newEqptsInfoAray); $indx++) {
+        $query = "INSERT INTO Equipments VALUES (?,?,?,?,?,?,?)";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param(
+            "sssssss",
+            $newEqptsInfoAray[$indx]["newEqptID"],
+            $newEqptsInfoAray[$indx]["newEqptName"],
+            $newEqptsInfoAray[$indx]["newEqptCls"],
+            $newEqptsInfoAray[$indx]["newEqptColg"],
+            $newEqptsInfoAray[$indx]["newEqptCre"],
+            $newEqptsInfoAray[$indx]["newEqptDesc"],
+            $eqptStat
+        );
+        $stmt->execute();
+    }
+
+    echo "<script>alert('成功导入" . count($newEqptsInfoAray) . "条新设备记录');</script>";
+}
+
+//释放结果集并关闭链接
+$stmt->free_result();
+$db->close();
 exit;
 ?>
