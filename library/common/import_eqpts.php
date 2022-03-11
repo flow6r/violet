21fcbf3a4e9312aacde42b560858ccb811a4d2e1
m66rk9t <m66rk9t@gmail.com>
2022-03-11 19:49:09 +0800

    Update library/common/import_eqpts.php

--- library/common/import_eqpts.php
+++ library/common/import_eqpts.php
@@ -1,6 +1,5 @@
 <?php
 /*批量导入设备信息的脚本*/
-
 //使用命名空间
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
@@ -12,6 +11,9 @@ $docRoot = $_SERVER["DOCUMENT_ROOT"];
 
 //获取当前时间信息
 $currDateTime = date("_YmdHis");
+
+//设置入库时间
+$newEqptCre = date("Y-m-d H:i:s");
 
 //检查文件上传错误
 if ($_FILES["newEqptsInfoTmpl"]["error"] === 1) {
@@ -28,6 +30,22 @@ if (
     exit;
 }
 
+//获取上传的照片的数量
+$newEqptsImgsCnt = count($_FILES["newEqptsImgs"]["name"]);
+
+//检查上传的设备图片文件类型
+for ($indx = 0; $indx < $newEqptsImgsCnt; $indx++) {
+    if (
+        $_FILES["newEqptsImgs"]["type"][$indx] != "image/bmp" &&
+        $_FILES["newEqptsImgs"]["type"][$indx] != "image/gif" &&
+        $_FILES["newEqptsImgs"]["type"][$indx] != "image/jpeg" &&
+        $_FILES["newEqptsImgs"]["type"][$indx] != "image/png"
+    ) {
+        echo "<script>alert('您上传的文件中包含非图片格式的文件，请检查无误后再执行上传操作');</script>";
+        exit;
+    }
+}
+
 //获取当前用户的姓名和角色信息
 session_start();
 $userRole = $_SESSION["userInfo"]["userRole"];
@@ -42,11 +60,11 @@ if ($_FILES["newEqptsInfoTmpl"]["type"] === "application/vnd.ms-excel") $extName
 else $extName = ".xlsx";
 
 //设置文件路径
-$trgtPath = $docRoot . "/data/upload/" . $baseName . $extName;
+$newEqptsInfoFile = $docRoot . "/data/upload/" . $baseName . $extName;
 
 //移动文件至指定目录
 if (is_uploaded_file($_FILES["newEqptsInfoTmpl"]["tmp_name"])) {
-    if (!move_uploaded_file($_FILES["newEqptsInfoTmpl"]["tmp_name"], $trgtPath)) {
+    if (!move_uploaded_file($_FILES["newEqptsInfoTmpl"]["tmp_name"], $newEqptsInfoFile)) {
         echo "<script>alert('移动设备信息文件时发生错误，请联系管理员并反馈问题');</script>";
         exit;
     }
@@ -54,27 +72,63 @@ if (is_uploaded_file($_FILES["newEqptsInfoTmpl"]["tmp_name"])) {
 
 //尝试读取文件
 $readXlsx = new Xlsx();
-$spreadsheet = $readXlsx->load($trgtPath);
+$spreadsheet = $readXlsx->load($newEqptsInfoFile);
 $newEqptsInfo = $spreadsheet->getActiveSheet(0);
 
 //获取数据行数
 $dataRows = $newEqptsInfo->getHighestRow() - 3;
 $begnRows = 4;
 
-//遍历数据表并初始化数组
-for ($outer = 0; $outer < $dataRows; $outer++, $begnRows++) {
-    $newEqptsInfoAray[$outer]["newEqptID"] = $newEqptsInfo->getCellByColumnAndRow(1, $begnRows)->getValue();
-    $newEqptsInfoAray[$outer]["newEqptName"] = $newEqptsInfo->getCellByColumnAndRow(2, $begnRows)->getValue();
-    $newEqptsInfoAray[$outer]["newEqptCls"] = $newEqptsInfo->getCellByColumnAndRow(3, $begnRows)->getValue();
-    $newEqptsInfoAray[$outer]["newEqptColg"] = $newEqptsInfo->getCellByColumnAndRow(4, $begnRows)->getValue();
-    $newEqptsInfoAray[$outer]["newEqptCre"] = $newEqptsInfo->getCellByColumnAndRow(5, $begnRows)->getValue();
-    $newEqptsInfoAray[$outer]["newEqptImgPath"] = "../images/eqpts/".$newEqptsInfoAray[$outer]["newEqptID"]."jpg";
-    $newEqptsInfoAray[$outer]["newEqptDesc"] = $newEqptsInfo->getCellByColumnAndRow(6, $begnRows)->getValue();
+//检查图片数量与新增设备记录数是否匹配
+if ($newEqptsImgsCnt != $dataRows) {
+    echo "<script>alert('您上传的新增设备记录数与设备图像文件数量不匹配，请检查无误后再执行上传操作');</script>";
+    unlink($newEqptsInfoFile);
+    exit;
+}
+
+//遍历数据表并初始化新增设备信息数组
+for ($indx = 0; $indx < $dataRows; $indx++, $begnRows++) {
+    $newEqptsInfoAray[$indx]["newEqptID"] = $newEqptsInfo->getCellByColumnAndRow(1, $begnRows)->getValue();
+    $newEqptsInfoAray[$indx]["newEqptName"] = $newEqptsInfo->getCellByColumnAndRow(2, $begnRows)->getValue();
+    $newEqptsInfoAray[$indx]["newEqptCls"] = $newEqptsInfo->getCellByColumnAndRow(3, $begnRows)->getValue();
+    $newEqptsInfoAray[$indx]["newEqptColg"] = $newEqptsInfo->getCellByColumnAndRow(4, $begnRows)->getValue();
+    $newEqptsInfoAray[$indx]["newEqptCre"] = $newEqptCre;
+    $newEqptsInfoAray[$indx]["newEqptDesc"] = $newEqptsInfo->getCellByColumnAndRow(5, $begnRows)->getValue();
+}
+
+//遍历上传的图片文件信息并初始化新增设备图片信息数组
+for ($indx = 0; $indx < $newEqptsImgsCnt; $indx++) {
+    $newEqptsImgsAray[$indx]["newEqptImgName"] = $_FILES["newEqptsImgs"]["name"][$indx];
+    $newEqptsImgsAray[$indx]["newEqptImgType"] = $_FILES["newEqptsImgs"]["type"][$indx];
+    $newEqptsImgsAray[$indx]["newEqptImgSize"] = $_FILES["newEqptsImgs"]["size"][$indx];
+}
+
+//遍历新增设备图片信息数组并设置对应设备的图片路径
+$newEqptsImgsArayLen = count($newEqptsImgsAray);
+for ($indx = 0; $indx < $newEqptsImgsArayLen; $indx++) {
+    $dotPosn = strpos($newEqptsImgsAray[$indx]["newEqptImgName"], ".");
+    $newEqptImgBaseName = substr($newEqptsImgsAray[$indx]["newEqptImgName"], 0, $dotPosn);
+    $newEqptImgExtName = substr($newEqptsImgsAray[$indx]["newEqptImgName"], $dotPosn);
+    $isEqptIDExist = false;
+    foreach ($newEqptsInfoAray as $key => $val) {
+        if ($newEqptsInfoAray[$key]["newEqptID"] === $newEqptImgBaseName) {
+            $newEqptsInfoAray[$key]["newEqptImgPath"] = "../images/eqpts/" . $newEqptsImgsAray[$indx]["newEqptImgName"];
+            $isEqptIDExist = true;
+        }
+    }
+    if (!$isEqptIDExist) {
+        echo "<script>alert('您上传的新增设备记录与设备图片中含有设备ID不匹配项，请检查无误后再执行上传操作');</script>";
+        unlink($newEqptsInfoFile);
+        exit;
+    }
 }
 
 //将数组保存为JSON数据并写入文件
 // $newEqptsInfoJSON = json_encode($newEqptsInfoAray, JSON_UNESCAPED_UNICODE);
 // file_put_contents("newEqptsInfo.json", $newEqptsInfoJSON);
+// $newEqptsInfoJSON = json_encode($newEqptsImgsAray, JSON_UNESCAPED_UNICODE);
+// file_put_contents("newEqptsImgs.json", $newEqptsInfoJSON);
+// exit;
 
 //引入数据库用户信息脚本
 switch ($userRole) {
@@ -103,7 +157,7 @@ for ($indx = 0; $indx < $dataRows; $indx++) {
     $stmt->store_result();
     if ($stmt->num_rows()) {
         echo "<script>alert('您所导入的设备信息中，包含已在库的设备，请检查无误后继续导入');</script>";
-        unlink($trgtPath);
+        unlink($newEqptsInfoFile);
         $isExist = true;
         break;
     }
@@ -122,8 +176,8 @@ if (!$isExist) {
             $newEqptsInfoAray[$indx]["newEqptName"],
             $newEqptsInfoAray[$indx]["newEqptCls"],
             $newEqptsInfoAray[$indx]["newEqptColg"],
-            $newEqptsInfoAray[$indx]["newEqptCre"],
             $newEqptsInfoAray[$indx]["newEqptImgPath"],
+            $newEqptsInfoAray[$indx]["newEqptCre"],
             $newEqptsInfoAray[$indx]["newEqptDesc"],
             $eqptStat
         );
@@ -131,6 +185,20 @@ if (!$isExist) {
     }
 
     echo "<script>alert('成功导入" . $dataRows . "条新设备记录');</script>";
+}
+
+//将上传的文件移动至指定位置
+for ($indx = 0; $indx < $newEqptsImgsArayLen; $indx++) {
+    $imgStoragePath = $docRoot . "/images/eqpts/";
+    if (is_uploaded_file($_FILES["newEqptsImgs"]["tmp_name"][$indx])) {
+        if (!move_uploaded_file(
+            $_FILES["newEqptsImgs"]["tmp_name"][$indx],
+            $imgStoragePath . $_FILES["newEqptsImgs"]["name"][$indx]
+        )) {
+            echo "<script>alert('移动设备图片文件时发生错误，请联系管理员并反馈问题');</script>";
+            exit;
+        }
+    }
 }
 
 //释放结果集并关闭链接
