3e768472ed5e41d8576af3bb8caff50b009aad3c
m66rk9t <m66rk9t@gmail.com>
2022-03-17 16:53:10 +0800

    Update scripts/common/panel.js

--- scripts/common/panel.js
+++ scripts/common/panel.js
@@ -294,7 +294,9 @@ function printQueryAppl() {
         "<tr><td><input type='text' id='searchItem' name='searchItem' placeholder='请输入待搜索的关键词' /></td>" +
         "<td><select id='searchType' name='searchType'><option value='applID'>申请记录ID</option>" +
         "<option value='userID'>申请用户ID</option><option value='applStat'>申请状态</option></select></td>" +
-        "<td><input type='button' id='queryUnrvwApplsBtn' name='queryUnrvwApplsBtn' value='查询' /></td></tr>" +
+        "<td><input type='button' id='queryUnrvwApplsBtn' name='queryUnrvwApplsBtn' value='查询' /></td>"+
+        "<td><input type='button' id='delApplsBtn' name='delApplsBtn' value='批量删除'/></td>" +
+        "<td><input type='button' id='procApplsBtn' name='procApplsBtn' value='批量处理'/></td></tr>" +
         "</table><table id='applRsltsTbl' name='applRsltsTbl'>" +
         "<tr id='applRsltsTblHead'><th width='50px'></th><th>申请记录ID</th><th>申请用户ID</th><th>申请详情</th><th>申请状态</th><th>操作</th></tr>" +
         "</table><table id='applsPageCtlTbl'>" +
@@ -302,6 +304,8 @@ function printQueryAppl() {
         "<td><input type='button' id='applsNextPage' value='下一页' /></td><td><input type='text' id='applsTrgtPage' placeholder='输入想要跳转的页数' /></td>" +
         "<td><input type='button' id='applsJump' value='跳转' /></td></tr></table></form></div>"
     );
+
+    if (userInfo.userRole === "学生") $("#content").find("#procApplsBtn").attr("style", "display: none");
 
     queryAppls(userInfo.userRole, "", "applStat");
 }
