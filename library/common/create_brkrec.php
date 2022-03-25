be62ca9a5293910bfac848d4c58c76d81ba9dab1
m66rk9t <m66rk9t@gmail.com>
2022-03-25 17:12:32 +0800

    Upload library/common/create_brkrec.php

---
+++ library/common/create_brkrec.php
@@ -0,0 +1,80 @@
+<?php
+/*报修单个设备的脚本*/
+//获取POST的请求数据
+$userID = $_POST["userID"];
+$userRole = $_POST["userRole"];
+$eqptID = $_POST["eqptID"];
+$lendBegn = $_POST["lendBegn"];
+
+//引入数据库用户信息脚本
+switch ($userRole) {
+    case "学生":
+        require_once("../dbuser/student.php");
+        break;
+    case "教师":
+        require_once("../dbuser/teacher.php");
+        break;
+    case "管理员":
+        require_once("../dbuser/admin.php");
+        break;
+}
+
+//连接数据库
+$db = mysqli_connect($dbServer, $dbUser, $dbUserPasswd, $dbName);
+if (mysqli_connect_error()) {
+    echo "连接数据库时发生错误，请联系管理员并反馈问题";
+    exit;
+}
+
+//检查设备借用记录状态
+$query = "SELECT E.EqptStat, L.LendStat FROM Equipments AS E, Lend AS L WHERE E.EqptID = L.EqptID AND L.UserID = ? AND L.EqptID = ? AND L.LendBegn = ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param("sss", $userID, $eqptID, $lendBegn);
+$stmt->execute();
+$stmt->store_result();
+if ($stmt->num_rows()) {
+    $stmt->bind_result($eqptStat, $lendStat);
+    $stmt->fetch();
+    if ($eqptStat != "已借出" && $lendStat != "未归还") {
+        echo "该设备无法报修，设备状态：" . $eqptStat . "，借用状态：" . $lendStat;
+        $stmt->free_result();
+        $db->close();
+        exit;
+    }
+} else {
+    echo "查询设备记录时发生错误，请联系管理员并反馈问题";
+    $stmt->free_result();
+    $db->close();
+    exit;
+}
+
+//更新设备借用记录状态
+$lendStat = "已归还";
+$lendRtn = date("Y-m-d H:i:s");
+$query = "UPDATE Lend SET LendStat = ?, LendRtn = ? WHERE UserID = ? AND EqptID = ? AND LendBegn = ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param("sssss", $lendStat, $lendRtn, $userID, $eqptID, $lendBegn);
+$stmt->execute();
+
+//更新设备借用状态
+$eqptStat = "已损坏";
+$query = "UPDATE Equipments SET EqptStat = ? WHERE EqptID = ?";
+$stmt = $db->prepare($query);
+$stmt->bind_param("ss", $eqptStat, $eqptID);
+$stmt->execute();
+
+//创建报修记录
+$stmt->free_result();
+$brkStat = "未处理";
+$brkCre = date("Y-m-d H:i:s");
+$query = "INSERT INTO Breakages VALUES(NULL,?,?,?,?,NUll,NULL)";
+$stmt = $db->prepare($query);
+$stmt->bind_param("ssss", $userID, $eqptID, $brkCre, $brkStat);
+$stmt->execute();
+
+echo "successful";
+
+//关闭链接
+$db->close();
+exit;
+?>
