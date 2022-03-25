46b072163bd58d44057f3885de26c2ad873aa610
m66rk9t <m66rk9t@gmail.com>
2022-03-25 19:04:01 +0800

    Update library/common/return_eqpt.php

--- library/common/return_eqpt.php
+++ library/common/return_eqpt.php
@@ -1,8 +1,8 @@
 <?php
 /*归还单个设备的脚本*/
 //获取POST请求的数据
-$userID = $_POST["userID"];
 $userRole = $_POST["userRole"];
+$lendID = intval($_POST["lendID"]);
 $eqptID = $_POST["eqptID"];
 
 //获取当前时间
@@ -29,28 +29,37 @@ if (mysqli_connect_error()) {
 }
 
 //查询数据库
-$lendStat = "未归还";
-$query = "SELECT LendBegn FROM Lend WHERE UserID = ? AND EqptID = ? AND LendStat = ?";
+$query = "SELECT LendStat FROM Lend WHERE LendID = ?";
 $stmt = $db->prepare($query);
-$stmt->bind_param("sss", $userID, $eqptID, $lendStat);
+$stmt->bind_param("i", $lendID);
 $stmt->execute();
 $stmt->store_result();
 if ($stmt->num_rows()) {
-    $stmt->bind_result($lendBegn);
+    $stmt->bind_result($lendStat);
     $stmt->fetch();
     $stmt->free_result();
-    $eqptStat = "未借出";
-    $lendStat = "已归还";
-    $query = "UPDATE Equipments E, Lend L SET E.EqptStat = ?, L.LendStat = ?, L.LendRtn = ? WHERE L.UserID = ? AND E.EqptID = L.EqptID AND L.EqptID = ? AND L.LendBegn = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bind_param("ssssss", $eqptStat, $lendStat, $lendRtn, $userID, $eqptID, $lendBegn);
-    $stmt->execute();
-} else echo "查询设备借用状态时发生错误，请联系管理员并反馈问题";
+    if ($lendStat === "已归还") {
+        echo "该设备已归还，请勿重复执行归还操作";
+        $db->close();
+        exit;
+    } else {
+        $eqptStat = "未借出";
+        $lendStat = "已归还";
+        $query = "UPDATE Equipments E, Lend L SET E.EqptStat = ?, L.LendStat = ?, L.LendRtn = ? WHERE L.LendID = ? AND E.EqptID = L.EqptID AND L.EqptID = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("sssis", $eqptStat, $lendStat, $lendRtn, $lendID, $eqptID);
+        $stmt->execute();
+    }
+} else {
+    echo "查询设备借用状态时发生错误，发生异常的借用记录ID为" . $lendStat . "请联系管理员并反馈问题";
+    $stmt->free_result();
+    $db->close();
+    exit;
+}
 
 echo "successful";
 
-//释放结果集并关闭链接
-$stmt->free_result();
+//关闭链接
 $db->close();
 exit;
 ?>
