75af6393abebec9cf5ef67a9cadbaca45f3d5cfa
m66rk9t <m66rk9t@gmail.com>
2022-03-28 17:01:13 +0800

    Update library/common/check_login.php

--- library/common/check_login.php
+++ library/common/check_login.php
@@ -3,21 +3,26 @@
 //获取POST请求的数据
 $userID = $_POST["userID"];
 $passwd = $_POST["userPasswd"];
+
 //引入数据库用户信息脚本
 require_once("../dbuser/user.php");
+
 //连接数据库
 $db = mysqli_connect($dbServer, $dbUser, $dbUserPasswd, $dbName);
 if (mysqli_connect_error()) {
     echo "连接数据库时发生错误，请联系管理员并反馈问题";
     exit;
 }
+
 //查询数据库
 $query = "SELECT * FROM Users where UserID = ?";
 $stmt = $db->prepare($query);
 $stmt->bind_param("s", $userID);
 $stmt->execute();
+
 //储存查询数据行
 $stmt->store_result();
+
 //检查查询结果
 if ($stmt->num_rows()) {
     //绑定查询结果参数
@@ -29,12 +34,9 @@ if ($stmt->num_rows()) {
         echo "valid";
         // 引入创建储存用户信息会话的脚本
         require_once("../session/user_login.php");
-    } else {
-        echo "密码错误，请检查密码";
-    }
-} else {
-    echo "该用户不存在，请注册后再登录";
-}
+    } else echo "密码错误，请检查密码";
+} else echo "该用户不存在，请注册后再登录";
+
 //释放结果集并关闭链接
 $stmt->free_result();
 $db->close();
