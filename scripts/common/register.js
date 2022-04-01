609b8fee43227ae6d30595e4418611eb09352ff9
m66rk9t <m66rk9t@gmail.com>
2022-04-01 18:36:42 +0800

    Update scripts/common/register.js

--- scripts/common/register.js
+++ scripts/common/register.js
@@ -1,5 +1,5 @@
 $("#userID").on("focusout", function checkUserID() {
-    var userID = $("#userID").val();
+    let userID = $("#userID").val();
 
     if (userID === "") $("#userID").attr("placeholder", "请输入用户ID");
     else $("#userID").removeAttr("placeholder");
@@ -7,22 +7,22 @@ $("#userID").on("focusout", function checkUserID() {
 });
 
 $("#userName").on("focusout", function checkUserName() {
-    var userName = $("#userName").val();
+    let userName = $("#userName").val();
 
     if (userName === "") $("#userName").attr("placeholder", "请输入姓名");
     else $("#userName").removeAttr("placeholder");
 });
 
 $("#userPasswd").on("focusout", function checkUserPasswd() {
-    var userPasswd = $("#userPasswd").val();
+    let userPasswd = $("#userPasswd").val();
 
     if (userPasswd === "") $("#userPasswd").attr("placeholder", "请输入6~18位密码");
     else $("#userPasswd").removeAttr("placeholder");
 });
 
 $("#retype").on("focusout", function checkRetype() {
-    var userPasswd = $("#userPasswd").val();
-    var retype = $("#retype").val();
+    let userPasswd = $("#userPasswd").val();
+    let retype = $("#retype").val();
 
     if (userPasswd != retype) $("#retype").attr("placeholder", "两次密码不一致");
     else $("#retype").removeAttr("placeholder");
@@ -30,7 +30,7 @@ $("#retype").on("focusout", function checkRetype() {
 
 $("#userEmail").on("focusout", function checkUserEmail() {
     /*需要改进，验证邮箱地址是否合法！*/
-    var userEmail = $("#userEmail").val();
+    let userEmail = $("#userEmail").val();
 
     if (userEmail === "") $("#userEmail").attr("placeholder", "请输合法的电子邮箱地址");
     else $("#userNuserEmailame").removeAttr("placeholder");
@@ -46,17 +46,17 @@ $("#userGen").on("focusin", function () {
 
 $("#userAdms").on("focusin", function queryYear() {
     $("#userAdms").empty();
-    var userAdms = $("#userAdms");
-    var currYear = new Date();
-    var yyyy = Number(currYear.getFullYear());
-    for (var lower = yyyy - 4; lower <= yyyy; lower++) {
+    let userAdms = $("#userAdms");
+    let currYear = new Date();
+    let yyyy = Number(currYear.getFullYear());
+    for (let lower = yyyy - 4; lower <= yyyy; lower++) {
         userAdms.append("<option value='" + lower + "'>" + lower + "</option>");
     }
 });
 
 $("#colgName").on("focusin", function queryMajor() {
     $("#colgName").find("option").remove();
-    var colgAbrv = $("#colgName");
+    let colgAbrv = $("#colgName");
     colgAbrv.append("<option value='tips'>请选择您所在的学院</option>");
     $.get("../../library/common/query_college.php", function (colgJSON) {
         $.each(JSON.parse(colgJSON), function (colgIndx, colgInfo) {
@@ -67,10 +67,10 @@ $("#colgName").on("focusin", function queryMajor() {
 
 $("#colgName").on("change", function queryMajor() {
     $("#mjrName").find("option").remove();
-    var colgAbrv = $("#colgName").val();
+    let colgAbrv = $("#colgName").val();
     if (colgAbrv != "tips") {
         $.post("../../library/common/query_major.php", { colgAbrv: colgAbrv }, function (majorJSON) {
-            var mjrName = $("#mjrName");
+            let mjrName = $("#mjrName");
             $.each(JSON.parse(majorJSON), function (majorIndx, majorInfo) {
                 mjrName.append(
                     "<option value='" + majorInfo.MjrAbrv + "'>" + majorInfo.MjrName + "</option>"
@@ -81,15 +81,15 @@ $("#colgName").on("change", function queryMajor() {
 });
 
 $("#createAccBtn").on("click", function createAccount() {
-    var userID = $("#userID").val();
-    var userName = $("#userName").val();
-    var userPasswd = $("#userPasswd").val();
-    var retype = $("#retype").val();
-    var userGen = $("#userGen").val();
-    var userEmail = $("#userEmail").val();
-    var colgAbrv = $("#colgName").val();
-    var userAdms = $("#userAdms").val();
-    var mjrAbrv = $("#mjrName").val();
+    let userID = $("#userID").val();
+    let userName = $("#userName").val();
+    let userPasswd = $("#userPasswd").val();
+    let retype = $("#retype").val();
+    let userGen = $("#userGen").val();
+    let userEmail = $("#userEmail").val();
+    let colgAbrv = $("#colgName").val();
+    let userAdms = $("#userAdms").val();
+    let mjrAbrv = $("#mjrName").val();
     if (userID != "" && userName != "" && userPasswd != "" && userPasswd === retype
         && userGen != "" && userEmail != "" && colgAbrv != "" && userAdms != "" && mjrAbrv != "") {
         $.post("../../library/common/create_account.php",
